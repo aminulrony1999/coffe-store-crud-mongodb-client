7fcedbf3d01274d6bb6b2889081cb25e16253708
aminulrony1999 <aminulrony1999@gmail.com>
2024-02-03 18:37:34 +0530

    coffe name added

--- src/components/AddCoffe.jsx
+++ src/components/AddCoffe.jsx
@@ -2,6 +2,19 @@ const AddCoffe = () => {
     return (
         <div>
             <h2>Add Coffe</h2>
+            <form>
+                <div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Coffe Name</span>
+                        </label>
+                        <label className="input-group">
+                            <span>Name</span>
+                            <input type="text" placeholder="Coffe name" className="input input-bordered" />
+                        </label>
+                    </div>
+                </div>
+            </form>
         </div>
     );
 };
