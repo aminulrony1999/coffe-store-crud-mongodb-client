1f4312349b328e77b672e73cf98a27eacca9e6ec
aminulrony1999 <aminulrony1999@gmail.com>
2024-02-11 16:31:15 +0530

    coffe update option added

--- src/components/CoffeCard.jsx
+++ src/components/CoffeCard.jsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 
 const CoffeCard = ({ coffe }) => {
-  const { _id, name, quantity, supplier, taste, category, details, photo } = coffe;
-  const handleDelete = _id => {
+  const { _id, name, quantity, supplier, taste, category, details, photo } =
+    coffe;
+  const handleDelete = (_id) => {
     Swal.fire({
       title: "Are you sure?",
       text: "You won't be able to revert this!",
@@ -10,18 +12,26 @@ const CoffeCard = ({ coffe }) => {
       showCancelButton: true,
       confirmButtonColor: "#3085d6",
       cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!"
+      confirmButtonText: "Yes, delete it!",
     }).then((result) => {
       if (result.isConfirmed) {
-        Swal.fire({
-          title: "Deleted!",
-          text: "Your file has been deleted.",
-          icon: "success"
-        });
+        fetch(`http://localhost:5000/coffe/${_id}`,{
+          method : 'DELETE'
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your coffe has been deleted.",
+                icon: "success",
+              });
+            }
+          });
       }
-    })
-  }
-  
+    });
+  };
+
   return (
     <div className="card card-side bg-base-100 shadow-xl">
       <figure>
@@ -29,16 +39,23 @@ const CoffeCard = ({ coffe }) => {
       </figure>
       <div className="flex justify-between w-full">
         <div>
-        <h2 className="card-title">Name : {name}</h2>
-        <p>{quantity}</p>
-        <p>{supplier}</p>
-        <p>{taste}</p>
+          <h2 className="card-title">Name : {name}</h2>
+          <p>{quantity}</p>
+          <p>{supplier}</p>
+          <p>{taste}</p>
         </div>
         <div className="card-actions justify-end">
           <div className="join join-vertical space-y-3">
             <button className="btn join-item">View</button>
-            <button className="btn join-item">Space</button>
-            <button onClick={() => handleDelete(_id)} className="btn join-item bg-orange-500">X</button>
+            <Link to={`updateCoffe/${_id}`}>
+            <button className="btn join-item">Edit</button>
+            </Link>
+            <button
+              onClick={() => handleDelete(_id)}
+              className="btn join-item bg-orange-500"
+            >
+              X
+            </button>
           </div>
         </div>
       </div>
